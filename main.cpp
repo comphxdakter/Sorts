4010fd25c399fcb6b6dfb441f6a356c2d5433db3
comphxdakter <49365744+comphxdakter@users.noreply.github.com>
2020-03-05 19:47:09 +0700

    Add bubble

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void swap(int &a,int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+void Bubble(int n,int *arr)
+{
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (arr[i] < arr[j])
+            {
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+}
+
+void FillArr(int *arr, int n)
+{
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand()%20;
+    }
+}
+void ShowArr(int *arr,int n)
+{
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 
 int main() {
-    
+    srand(time(0));
+
+    int n = 10;
+    int *arr = new int(n);
+    FillArr(arr,n);
+    ShowArr(arr,n);
+
+    Bubble(n,arr);
+    ShowArr(arr,n);
+
     return 0;
 }
